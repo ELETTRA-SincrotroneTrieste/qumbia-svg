d6aae1a1f38b69e34b4b293d65aab2bbddc889bc
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2020-02-12 16:58:05 +0100

    added qusvgsynoptic/ example

---
+++ examples/qusvgsynoptic/src/qusvgsynoptic.cpp
@@ -0,0 +1,131 @@
+#include "qusvgsynoptic.h"
+
+#include <cumbiapool.h>
+#include <cumbiaepics.h>
+#include <cumbiatango.h>
+#include <cuepcontrolsreader.h>
+#include <cuepcontrolswriter.h>
+#include <cutcontrolsreader.h>
+#include <cutcontrolswriter.h>
+#include <cutango-world.h>
+#include <cuepics-world.h>
+#include <cuthreadfactoryimpl.h>
+#include <cuserviceprovider.h>
+#include <qthreadseventbridgefactory.h>
+#include <cumacros.h>
+
+#include <QMessageBox>
+#include <QGridLayout>
+
+// cumbia svg includes
+#include <qusvgview.h>
+#include <qusvg.h>
+#include <QtDebug>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QDateTime>
+
+#include <QSettings> // save line edit values
+
+QuSvgSynoptic::QuSvgSynoptic(CumbiaPool *cumbia_pool, QWidget *parent) :
+    QWidget(parent)
+{
+    cu_pool = cumbia_pool;
+    // setup Cumbia pool and register cumbia implementations for tango and epics
+    CumbiaEpics* cuep = new CumbiaEpics(new CuThreadFactoryImpl(), new QThreadsEventBridgeFactory());
+    CumbiaTango* cuta = new CumbiaTango(new CuThreadFactoryImpl(), new QThreadsEventBridgeFactory());
+    cu_pool->registerCumbiaImpl("tango", cuta);
+    cu_pool->registerCumbiaImpl("epics", cuep);
+    m_ctrl_factory_pool.registerImpl("tango", CuTReaderFactory());
+    m_ctrl_factory_pool.registerImpl("tango", CuTWriterFactory());
+    m_ctrl_factory_pool.registerImpl("epics", CuEpReaderFactory());
+    m_ctrl_factory_pool.registerImpl("epics", CuEpWriterFactory());
+
+    CuTangoWorld tw;
+    m_ctrl_factory_pool.setSrcPatterns("tango", tw.srcPatterns());
+    cu_pool->setSrcPatterns("tango", tw.srcPatterns());
+    CuEpicsWorld ew;
+    m_ctrl_factory_pool.setSrcPatterns("epics", ew.srcPatterns());
+    cu_pool->setSrcPatterns("epics", ew.srcPatterns());
+
+    // log
+    cuta->getServiceProvider()->registerService(CuServices::Log, new CuLog(&m_log_impl));
+    cuep->getServiceProvider()->registerService(CuServices::Log, new CuLog(&m_log_impl));
+
+//    ui->setupUi(this, cu_pool, m_ctrl_factory_pool);
+    QGridLayout *lo = new QGridLayout(this);
+    QuSvgView *svgview = new QuSvgView(this);
+    m_qu_svg = new QuSvg(svgview);
+    m_qu_svg->init(cu_pool, m_ctrl_factory_pool);
+    lo->addWidget(svgview, 0,0, 5, 5 );
+
+    QLabel *l = new QLabel("Set attribute", this);
+    QLineEdit *le_id = new QLineEdit(this);
+    le_id->setObjectName("le_id");
+    QLineEdit *le_attnam = new QLineEdit(this);
+    le_attnam->setObjectName("le_attnam");
+    QLineEdit *le_attval = new QLineEdit(this);
+    le_attval->setObjectName("le_attval");
+    QPushButton *pb = new QPushButton("APPLY", this);
+    lo->addWidget(l, 5, 0, 1, 1);
+    lo->addWidget(le_id, 5, 1, 1, 1);
+    lo->addWidget(le_attnam, 5, 2, 1, 1);
+    lo->addWidget(le_attval, 5, 3, 1, 1);
+    lo->addWidget(pb, 5, 4, 1, 1);
+    connect(pb, SIGNAL(clicked()), this, SLOT(applyClicked()));
+
+    QSettings s;
+    le_id->setText(s.value("id").toString());
+    le_attnam->setText(s.value("attnam").toString());
+    le_attval->setText(s.value("attval").toString());
+
+    bool ok = m_qu_svg->loadFile(":src/drawing.svg");
+    if(!ok) {
+        QMessageBox::critical(this, "Error loading svg file", m_qu_svg->message());
+    }
+    else
+        m_qu_svg->addDataListener(this);
+
+}
+
+QuSvgSynoptic::~QuSvgSynoptic()
+{
+    QString id = findChild<QLineEdit *>("le_id")->text();
+    QString attn = findChild<QLineEdit *>("le_attnam")->text();
+    QString attv = findChild<QLineEdit *>("le_attval")->text();
+    QSettings s;
+    s.setValue("id", id);
+    s.setValue("attnam", attn);
+    s.setValue("attval", attv);
+}
+
+void QuSvgSynoptic::applyClicked()
+{
+    QString id = findChild<QLineEdit *>("le_id")->text();
+    QString attn = findChild<QLineEdit *>("le_attnam")->text();
+    QString attv = findChild<QLineEdit *>("le_attval")->text();
+    QuDom dom = m_qu_svg->quDom();
+    QuDomElement dome(dom);
+    QuDomElement de = dome[id];
+    if(de.isNull()) {
+        QMessageBox::information(this, "Could not change attribute",
+                                 QString("Failed to set value \"%1\" on attribute"
+                                         "\'%2\" element id \"%3\"").arg(attv)
+                                 .arg(attn).arg(id));
+    }
+    else
+        de.a(attn, attv);
+}
+
+bool QuSvgSynoptic::onUpdate(const QuSvgResultData &res, QuDom *qudom)
+{
+    printf("QuSvgSynoptic.onUpdate [\e[1;32mDATA\e[0m]: %s %s [\e[1;36mRECIPIENT\e[0m]: "
+        "id: \"\e[0;32m%s\e[0m\" attribute: \"%s/\e[1;36m%s\e[0m\"\n", qstoc(QDateTime::fromMSecsSinceEpoch(
+                     res.data["timestamp_ms"].toLongInt()).toString()),
+            res.data["value"].toString().c_str(),
+         qstoc(res.id), qstoc(res.attribute), qstoc(res.property()));
+
+    printf("\e[1;35m: returning false: automatic processing...\e[0m\n");
+    return false;
+}
---
+++ examples/qusvgsynoptic/src/qusvgsynoptic.h
@@ -0,0 +1,38 @@
+#ifndef QuSvgExample_H
+#define QuSvgExample_H
+
+#include <QWidget>
+#include <qulogimpl.h>
+#include <cucontrolsfactorypool.h>
+#include <cumbiatango.h>
+#include <cumbiaepics.h>
+
+#include <qusvgdatalistener.h>
+
+class CuData;
+class CumbiaPool;
+class QuSvg;
+class QuSvgSynoptic : public QWidget, public QuSvgDataListener
+{
+    Q_OBJECT
+
+public:
+    explicit QuSvgSynoptic(CumbiaPool *cu_p, QWidget *parent = 0);
+    ~QuSvgSynoptic();
+
+private slots:
+    void applyClicked();
+
+private:
+    QuSvg *m_qu_svg;
+
+    CumbiaPool *cu_pool;
+    QuLogImpl m_log_impl;
+    CuControlsFactoryPool m_ctrl_factory_pool;
+
+    // QuSvgDataListener interface
+public:
+    bool onUpdate(const QuSvgResultData &res, QuDom *qudom);
+};
+
+#endif // QuSvgExample_H
